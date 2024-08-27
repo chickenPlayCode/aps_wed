cd2e1eb48317c3477d796c9402671e7aa18e8f98
HoangMmap <145683102+chickenPlayCode@users.noreply.github.com>
2024-08-27 15:02:59 +0700

    len bai 2 3

---
+++ BaiTap02-/BaiTap02-/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap02_.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
